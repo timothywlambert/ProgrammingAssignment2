f1b8558b3feace0da0679e3b3927a94e4940ba2a
timothywlambert <timothywlambert@hotmail.com>
2017-04-30 19:25:02 -0400

    Update cachematrix.R

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,37 @@
-## Put comments here that give an overall description of what your
-## functions do
+## These function will calculate the inverse of a matrix and store the result in cache.
 
-## Write a short comment describing this function
+## makeCacheMatrix opens up MASS package to calculate the inverse of a matrix using the ginv function and stores it in an object
+
+## Create a matrix first and assign it variable x
+## Assign makeCacheMatrix to variable y and run
 
 makeCacheMatrix <- function(x = matrix()) {
-
+  library(MASS)
+  m <- NULL
+  set <- function(y) {
+    x <<- y
+    m <<- NULL
+  }
+  get <- function() x
+  
+  setinverse <- function(ginv) m <<- ginv
+  getinverse <- function() m
+  list(set = set, get = get,
+       setinverse = setinverse,
+       getinverse = getinverse) 
 }
 
-
-## Write a short comment describing this function
+## This function calculates the inverse of matrix
+## Assign this function to z and run
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+  m <- x$getinverse()
+  if(!is.null(m)) {
+    message("getting cached data")
+    return(m)
+  }
+  data <- x$get()
+  m <- ginv(data, ...)
+  x$setinverse(m)
+  m
 }
